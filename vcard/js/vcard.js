d3885b97f33689ab82e35e9f7955ead32cfbf453
Rahmatt <iradaturrahmat@gmail.com>
2021-10-09 21:37:18 +0700

    Create vcard.js

---
+++ vcard/js/vcard.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+    $('.portfolio-gallery .entry').colorbox({
+        scalePhotos : true,
+        scrolling: false,
+        rel:'gal',
+        returnFocus : false,
+
+        maxWidth : '80%',
+        maxHeight : '80%'
+    });
+})
