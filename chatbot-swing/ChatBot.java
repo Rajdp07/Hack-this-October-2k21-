4807fcd7cb1216a820e09cb4629f38b2d9dd771b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-10-18 21:24:51 +0530

    feat (chatbot) : chatbot using swing added

---
+++ chatbot-swing/ChatBot.java
@@ -0,0 +1,15 @@
+package ChatBot;
+/**
+ * 
+ * @author Soumyadip Chowdhury
+ * @github soumyadip007
+ *
+ */
+public class ChatBot {
+
+	public static void main(String[] args) {
+		ChatBotMain obj=new ChatBotMain();
+		obj.setTitle("ChatBot(Team-Cognito)");
+		obj.setVisible(true);
+	}
+}
